d0d7c5500cad48b7297c7d87876969553311724f
ersan <ersan@MacBook-Air-von-ersan.local>
2026-02-21 18:17:18 +0100

    feat: added primary pages for navigation, colors, fonts, and settings

---
+++ src/app/components/wildcard-page/wildcard-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wildcard-page',
+  imports: [],
+  templateUrl: './wildcard-page.html',
+  styleUrl: './scss/wildcard-page.scss',
+})
+export class WildcardPage {
+
+}
