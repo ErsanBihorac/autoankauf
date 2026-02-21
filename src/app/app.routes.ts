a7be2ebeb49a49e612d832fbd3bfc14ce9f68bf2
ersan <ersan@MacBook-Air-von-ersan.local>
2026-02-21 18:18:16 +0100

    feat: added primary pages, colors, fonts and settings

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LandingPage } from './components/landing-page/landing-page';
+import { WildcardPage } from './components/wildcard-page/wildcard-page';
+import { ImprintPage } from './components/imprint-page/imprint-page';
+import { PrivacyPolicyPage } from './components/privacy-policy-page/privacy-policy-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: LandingPage},
+    {path: "**", component: WildcardPage},
+    {path: "imprint", component: ImprintPage},
+    {path: "privacy-policy", component: PrivacyPolicyPage},
+];
