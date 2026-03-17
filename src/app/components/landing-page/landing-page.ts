d14ad8592fc65c43519c87fab5f6074e4772c682
ersan <ersan@MacBook-Air-von-ersan.local>
2026-03-17 18:22:55 +0100

    feat: created footer

--- src/app/components/landing-page/landing-page.ts
+++ src/app/components/landing-page/landing-page.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Nav } from '../nav/nav';
 import { Intro } from '../intro/intro';
 import { CarDetails } from '../car-details/car-details';
+import { Footer } from "../footer/footer";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [Nav, Intro, CarDetails],
+  imports: [Nav, Intro, CarDetails, Footer],
   templateUrl: './landing-page.html',
   styleUrl: './scss/landing-page.scss',
 })
